3015cb582e78128522a97371e5ed492d64b2f0e8
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-01 12:37:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-   let input = document.getElementById("new-task-description");
+  let input = document.getElementById("new-task-description");
   let submit = document.getElementById("submit");
   submit.onclick = function (e) {
     let tasks = document.getElementById("tasks");
