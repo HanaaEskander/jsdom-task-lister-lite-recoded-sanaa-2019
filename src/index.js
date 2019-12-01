9e7f51b4cba83cdc50a9de50f8fb2d19fdd2943d
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-01 12:45:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const input = document.getElementById('new-task-description');
+  const input = document.getElementById('new-task-description').value;
   const form = document.getElementById('create-task-form');
   const tasks = document.getElementById("tasks");
   const priority = document.getElementById('priority');
   
   
   function list(input) {
-    let ul = document.getElementById('tasks');
+    
     let li = document.createElement('li');
     li.innerHTML = input;
-    ul.appendChild(li);
+    tasks.appendChild(li);
   }
   
-let a=document.body.getElementById("#create-task-form input[type='submit']")
-a.addEventListener("onClick", function (e) {
+
+form.addEventListener("onClick", function (e) {
 e.preventDefault();
-let input = document.getElementById('new-task-description').value;
+
 list(input);
 input.value = "";
 
