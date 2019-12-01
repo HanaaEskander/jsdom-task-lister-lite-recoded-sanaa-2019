e0b09a5ae3a084615ca2a7d9c57d79876bc070c2
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-01 12:34:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const input = document.getElementById('new-task-description');
-  const form = document.getElementById('create-task-form');
-  const tasks = document.getElementById("tasks");
-  const priority = document.getElementById('priority');
-
-  form.addEventListener('submit',formSubmit);
-
-  function rmvTask(e){
-    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-  }
-  
-  function formSubmit(e){
+   let input = document.getElementById("new-task-description");
+  let submit = document.getElementById("submit");
+  submit.onclick = function (e) {
+    let tasks = document.getElementById("tasks");
+    let li = document.createElement("li")
+    li.innerHTML = input.value;
+    tasks.appendChild(li);
     e.preventDefault();
-    if(input.value != ""){
-      let option = priority.options[priority.selectedIndex];
-      let sortChar = option.getAttribute('data-sort');
-      let li = document.createElement('li');
-      let btn = document.createElement('button');
-      li.textContent = input.value + ' ';
-      li.setAttribute('data-sort',sortChar);
-      li.style.color = priority.value;
-      btn.innerHTML = 'X';
-      btn.addEventListener('click',deleteTask);
-      li.appendChild(btn);
-      tasks.appendChild(li);
-      input.value = "";
-    }
   }
 
 });
